b5d56e5d88daade67a714a96a9a4f34809a09aab
3ld0n <eldon.umphrey@gmail.com>
2015-09-01 13:15:12 -0500

    initial commit

---
+++ src/awsController.js
@@ -0,0 +1,41 @@
+var express = require('express')
+    , http = require('http')
+    ,_     = require('underscore');
+var app = express();
+
+var AWS = require('aws-sdk')
+AWS.config.update({accessKeyId: 'AKIAIQZNAE6PBV6RJESQ', secretAccessKey: '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', region: 'us-west-2'})
+
+
+http.createServer(app).listen(5000, function(){
+    console.log('Express server listening on port ' + 5000);
+});
+
+app.get('/', function (req, res) {
+res.send('Hello WORd/n');
+});
+
+
+app.get('/createECSInstance', function(req, rsp){
+    "use strict";
+
+    var ec2 = new AWS.EC2();
+
+
+    var params = {
+        ImageId: 'ami-cb584dfb',  // amazon linux ami ecs optimized
+        InstanceType: 't2.micro',
+        MinCount: 1, MaxCount: 1
+    };
+
+    ec2.runInstances(params, function(err, data) {
+        if (err) { rsp.status("Could not create instance").send(err); return; }
+
+        var instanceID = data.Instances[0].InstanceID;
+        rsp.send("Created instance", instanceID );
+
+        });
+
+});
+
+
